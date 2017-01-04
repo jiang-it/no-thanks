0168ca04244b50d9971cb90a53f09fd0ff71b8ee
TJiang <thomasjiang@college.harvard.edu>
2017-01-04 16:29:30 -0500

    Added connecting by the client

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
 http.listen(port, function(){
