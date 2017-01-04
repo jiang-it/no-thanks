f0fbc1bf79605fe06e5e74b3edb90dfb41abaa0c
TJiang <thomasjiang@college.harvard.edu>
2017-01-04 17:23:11 -0500

    Add messaging

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+    console.log('hmm');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    console.log('huh');
 });
 
 http.listen(port, function(){
