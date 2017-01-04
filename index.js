34e2b8e1818b1e88289ae7a9a92f0ca726393875
TJiang <thomasjiang@college.harvard.edu>
2017-01-04 16:50:19 -0500

    Factor out the Javascript in index.html

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 port = process.env.PORT || 3000;
+
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
