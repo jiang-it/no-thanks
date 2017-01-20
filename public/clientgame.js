9415c41d4bc0d0b3be428e7f0c1304c0097e9246
jiang-it <chaos488@gmail.com>
2017-01-20 17:59:01 -0500

    Test input field on mobile

--- public/clientgame.js
+++ public/clientgame.js
@@ -1,15 +1,27 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 buttonWidth = 193;
-buttonHeight = 71
+buttonHeight = 71;
 
 function preload() {
+	game.plugins.add(PhaserInput.Plugin);
 	game.load.spritesheet('button', '/public/button_sprite_sheet.png', buttonWidth, buttonHeight);
 }
 
 function create() {
-    passButton = game.add.button(game.world.centerX - buttonWidth, 400, 'button', actionOnClick, this, 2, 1, 0);
-    takeButton = game.add.button(game.world.centerX, 400, 'button', actionOnClick, this, 2, 1, 0);
+    var passButton = game.add.button(game.world.centerX - buttonWidth, 400, 'button', actionOnClick, this, 2, 1, 0);
+    var takeButton = game.add.button(game.world.centerX, 400, 'button', actionOnClick, this, 2, 1, 0);
+	var password = game.add.inputField(10, 90, {
+		font: '40px Arial',
+		fill: '#212121',
+		fontWeight: 'bold',
+		width: 150,
+		padding: 8,
+		borderWidth: 1,
+		borderColor: '#000',
+		borderRadius: 6,
+		placeHolder: 'GameID',
+	});
 }
 
 function update() {
