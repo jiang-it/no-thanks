e66f496a9505f1161ec7b63da93c994e148a5efd
jiang-it <chaos488@gmail.com>
2017-01-21 00:18:42 -0500

    Test buttons and input

--- public/clientgame.js
+++ public/clientgame.js
@@ -9,24 +9,55 @@ function preload() {
 }
 
 function create() {
-    var passButton = game.add.button(game.world.centerX - buttonWidth, 400, 'button', actionOnClick, this, 2, 1, 0);
-    var takeButton = game.add.button(game.world.centerX, 400, 'button', actionOnClick, this, 2, 1, 0);
-	var password = game.add.inputField(10, 90, {
+	// This is the start
+	// Maybe some name inputs? I guess?
+	var createButton = game.add.button(game.world.centerX, 100, 'button', onCreate, this, 2, 1, 0);
+
+	var gameIDField = game.add.inputField(game.world.centerX - buttonWidth, 200, {
 		font: '40px Arial',
 		fill: '#212121',
 		fontWeight: 'bold',
-		width: 150,
+		width: buttonWidth,
 		padding: 8,
 		borderWidth: 1,
 		borderColor: '#000',
 		borderRadius: 6,
 		placeHolder: 'GameID',
 	});
+	var joinButton = game.add.button(game.world.centerX, 200, 'button', onJoin, this, 2, 1, 0);
+	
+	// Before a game has been started
+	var startButton = game.add.button(game.world.centerX - buttonWidth, 300, 'button', onStart, this, 2, 1, 0);
+    var leaveButton = game.add.button(game.world.centerX, 300, 'button', onLeave, this, 2, 1, 0);
+
+	// While a game is happening
+    var passButton = game.add.button(game.world.centerX - buttonWidth, 400, 'button', onPass, this, 2, 1, 0);
+    var takeButton = game.add.button(game.world.centerX, 400, 'button', onTake, this, 2, 1, 0);
 }
 
 function update() {
 }
 
-function actionOnClick() {
-	
+function onCreate() {
+	socket.emit('create');
+}
+
+function onJoin() {
+	socket.emit('join', {gameID : gameIDField.value});
+}
+
+function onStart() {
+	socket.emit('start');
+}
+
+function onLeave() {
+	socket.emit('leave');
+}
+
+function onPass() {
+	socket.emit('pass');
+}
+
+function onTake() {
+	socket.emit('take');
 }
